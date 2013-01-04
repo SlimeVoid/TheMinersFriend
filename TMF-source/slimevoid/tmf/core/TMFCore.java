e0864ef6990e988c476e9d677c06fee1581e6a61
Eurymachus <reflex_ion@hotmail.com>
2013-01-04 19:45:01 +0000

    Start of the Miners Hat

--- TMF-source/slimevoid/tmf/core/TMFCore.java
+++ TMF-source/slimevoid/tmf/core/TMFCore.java
@@ -2,13 +2,25 @@ package slimevoid.tmf.core;
 
 import java.io.File;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+
+import slimevoid.tmf.items.ItemMinersHat;
+
 import eurysmods.api.ICommonProxy;
 
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.Configuration;
 
 public class TMFCore {
 	public static File configFile;
 	public static Configuration configuration;
+	public static Item miningHelmetIron;
+	public static Item miningHelmetGold;
+	public static Item miningHelmetDiamond;
+	public static Item miningHelmetLamp;
+	public static int miningHelmetIronId, miningHelmetGoldId, miningHelmetDiamondId;
 	public static String loggerLevel = "INFO";
 
 	public static void initialize(ICommonProxy proxy) {
@@ -16,12 +28,51 @@ public class TMFCore {
 	}
 
 	public static void addItems() {
+		miningHelmetIron = new ItemMinersHat(miningHelmetIronId, EnumArmorMaterial.IRON, 2, 0).setItemName("ironMiningHelmet").setIconCoord(0, 0);
+		miningHelmetGold = new ItemMinersHat(miningHelmetGoldId, EnumArmorMaterial.GOLD, 4, 0).setItemName("goldMiningHelmet").setIconCoord(0, 1);
+		miningHelmetDiamond = new ItemMinersHat(miningHelmetDiamondId, EnumArmorMaterial.DIAMOND, 3, 0).setItemName("diamondMiningHelmet").setIconCoord(0, 2);
 	}
 
 	public static void addNames() {
+		GameRegistry.registerItem(miningHelmetIron, "Iron Mining Helmet");
+		GameRegistry.registerItem(miningHelmetGold, "Gold Mining Helmet");
+		GameRegistry.registerItem(miningHelmetDiamond, "Diamond Mining Helmet");
 	}
 
 	public static void addRecipes() {
+		GameRegistry.addRecipe(
+				new ItemStack(miningHelmetIron),
+				new Object[] {
+					"X",
+					"Y",
+					Character.valueOf('X'),
+					miningHelmetLamp,
+					Character.valueOf('Y'),
+					Item.helmetSteel
+				}
+		);
+		GameRegistry.addRecipe(
+				new ItemStack(miningHelmetGold),
+				new Object[] {
+					"X",
+					"Y",
+					Character.valueOf('X'),
+					miningHelmetLamp,
+					Character.valueOf('Y'),
+					Item.helmetGold
+				}
+		);
+		GameRegistry.addRecipe(
+				new ItemStack(miningHelmetDiamond),
+				new Object[] {
+					"X",
+					"Y",
+					Character.valueOf('X'),
+					miningHelmetLamp,
+					Character.valueOf('Y'),
+					Item.helmetDiamond
+				}
+		);
 	}
 
 	public static int configurationProperties() {
@@ -30,6 +81,18 @@ public class TMFCore {
 				Configuration.CATEGORY_GENERAL,
 				"loggerLevel",
 				"INFO").value);
+		miningHelmetIronId = Integer.valueOf(configuration.get(
+				Configuration.CATEGORY_ITEM,
+				"ironMinersHat",
+				15000).value);
+		miningHelmetGoldId = Integer.valueOf(configuration.get(
+				Configuration.CATEGORY_ITEM,
+				"goldMinersHat",
+				15001).value);
+		miningHelmetDiamondId = Integer.valueOf(configuration.get(
+				Configuration.CATEGORY_ITEM,
+				"diamondMinersHat",
+				15002).value);
 		configuration.save();
 		LoggerTMF.getInstance("LittleBlocksConfig").setFilterLevel(loggerLevel);
 		return 0;
---
+++ TMF-source/slimevoid/tmf/items/ItemMinersHat.java
@@ -0,0 +1,30 @@
+package slimevoid.tmf.items;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.ItemArmor;
+
+public class ItemMinersHat extends ItemArmor {
+
+	public ItemMinersHat(int itemID, EnumArmorMaterial material, int renderIndex, int armorType) {
+		super(itemID, material, renderIndex, armorType);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getIconFromDamageForRenderPass(int damage, int renderPass) {
+		return renderPass == 1 ? this.iconIndex : super.getIconFromDamageForRenderPass(damage, renderPass);
+	}
+	
+	@Override
+    public int getIconFromDamage(int par1)
+    {
+        return this.iconIndex;
+    }
+
+	@Override
+	public String getTextureFile() {
+		return "/theminersfriend/gui/items.png";
+	}
+}
