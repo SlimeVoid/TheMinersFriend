2a77dd867ee93b35f8bde49feef11b65f2cd7ba4
Eurymachus <reflex_ion@hotmail.com>
2013-01-20 04:05:16 +0000

    Faffing with Naming

--- TMF-source/slimevoid/tmf/core/lib/NamingLib.java
+++ TMF-source/slimevoid/tmf/core/lib/NamingLib.java
@@ -6,15 +6,21 @@ public class NamingLib {
 
 	public static final String MOTION_SENSOR = "Motion Sensor";
 	public static final String MOTION_SENSOR_SETTINGS = "Sensor Settings";
+	
+	private static final String ARKITE = "Arkite";
+	private static final String BISTITE = "Bistite";
+	private static final String CROKERE = "Crokere";
+	private static final String DERNITE = "Dernite";
+	private static final String EGIOCLASE = "Egioclase";
 
-	public static final String ORE_ARKITE = "Arkite Ore";
-
-	public static final String ORE_BISTITE = "Bistite Ore";
-
-	public static final String ORE_CROKERE = "Crokere Ore";
-
-	public static final String ORE_DERNITE = "Dernite Ore";
-
-	public static final String ORE_EGIOCLASE = "Egioclase Ore";
-
+	private static final String ORE =  "Ore";
+	public static final String ORE_ARKITE = makeName(ARKITE, ORE);
+	public static final String ORE_BISTITE = makeName(BISTITE, ORE);
+	public static final String ORE_CROKERE = makeName(CROKERE, ORE);
+	public static final String ORE_DERNITE = makeName(DERNITE, ORE);
+	public static final String ORE_EGIOCLASE = makeName(EGIOCLASE, ORE);
+	
+	private static String makeName(String str1, String str2) {
+		return str1 + " " + str2;
+	}
 }
