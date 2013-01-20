4500cda5d490ae43181bf67eda954a0e11b622fd
Eurymachus <reflex_ion@hotmail.com>
2013-01-20 01:45:12 +0000

    Basic Ore class WorldGeneration and registerTMFOres

--- TMF-source/slimevoid/tmf/blocks/ores/BlockTMFOre.java
+++ TMF-source/slimevoid/tmf/blocks/ores/BlockTMFOre.java
@@ -3,9 +3,19 @@ package slimevoid.tmf.blocks.ores;
 import net.minecraft.block.BlockOre;
 
 public class BlockTMFOre extends BlockOre {
+	
+	public int 
+		spawnLevel,
+		spawnRate,
+		spawnSize,
+		lightLevel;
 
-	public BlockTMFOre(int id, int blockIndexInTexture) {
+	public BlockTMFOre(int id, int blockIndexInTexture, int spawnLevel, int spawnRate, int spawnSize, int lightLevel) {
 		super(id, blockIndexInTexture);
+		this.spawnLevel = spawnLevel;
+		this.spawnRate = spawnRate;
+		this.spawnSize = spawnSize;
+		this.lightLevel = lightLevel;
 	}
 
 }
