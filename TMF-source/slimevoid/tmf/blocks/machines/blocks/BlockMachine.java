9422f8f19889660b04a69ecad9701f9c4ce41350
ali4z <howdy1@gmail.com>
2013-01-22 00:15:57 +0100

    Drop content on breaking machine blocks.

--- TMF-source/slimevoid/tmf/blocks/machines/blocks/BlockMachine.java
+++ TMF-source/slimevoid/tmf/blocks/machines/blocks/BlockMachine.java
@@ -1,14 +1,22 @@
 package slimevoid.tmf.blocks.machines.blocks;
 
+import java.util.Random;
+
+import slimevoid.tmf.blocks.machines.tileentities.TileEntityMachine;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public abstract class BlockMachine extends BlockContainer {
-
+	private Random rand = new Random();
+	
 	public BlockMachine(int id, int texX, int texY, boolean isActive) {
 		super(id, Material.rock);
         this.blockIndexInTexture = texX+(texY*16);
@@ -72,6 +80,51 @@ public abstract class BlockMachine extends BlockContainer {
 
 	@Override
 	public void breakBlock(World world, int x, int y, int z, int l, int m) {
+		TileEntityMachine tile = (TileEntityMachine)world.getBlockTileEntity(x, y, z);
+		
+		if (tile != null) {
+			for (int i = 0; i < tile.getSizeInventory(); ++i) {
+				ItemStack stack = tile.getStackInSlot(i);
+				
+				if (stack != null) {
+					float rndX = this.rand.nextFloat() * 0.8F + 0.1F;
+					float rndY = this.rand.nextFloat() * 0.8F + 0.1F;
+					float rndZ = this.rand.nextFloat() * 0.8F + 0.1F;
+					
+					while (stack.stackSize > 0) {
+						int rndSize = this.rand.nextInt(21) + 10;
+						
+						if (rndSize > stack.stackSize) {
+							rndSize = stack.stackSize;
+						}
+						
+						stack.stackSize -= rndSize;
+						EntityItem entity = new EntityItem(
+								world, 
+								(double)((float)x + rndX), 
+								(double)((float)y + rndY), 
+								(double)((float)z + rndZ), 
+								new ItemStack(
+										stack.itemID, 
+										rndSize, 
+										stack.getItemDamage()
+								)
+						);
+						
+						if (stack.hasTagCompound()) {
+							entity.func_92014_d().setTagCompound((NBTTagCompound)stack.getTagCompound().copy());
+						}
+						
+						float multi = 0.05F;
+						entity.motionX = (double)((float)this.rand.nextGaussian() * multi);
+						entity.motionY = (double)((float)this.rand.nextGaussian() * multi + 0.2F);
+						entity.motionZ = (double)((float)this.rand.nextGaussian() * multi);
+						world.spawnEntityInWorld(entity);
+					}
+				}
+			}
+		}
+		
 		super.breakBlock(world, x, y, z, l, m);
 	}
 }
