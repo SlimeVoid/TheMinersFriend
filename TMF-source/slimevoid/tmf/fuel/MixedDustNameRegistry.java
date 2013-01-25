489e00fac30fdf6162fbe58ffc2214a517f7c802
ali4z <howdy1@gmail.com>
2013-01-25 15:32:40 +0100

    Chemical default names for mixed dusts.

--- TMF-source/slimevoid/tmf/fuel/MixedDustNameRegistry.java
+++ TMF-source/slimevoid/tmf/fuel/MixedDustNameRegistry.java
@@ -45,6 +45,89 @@ public class MixedDustNameRegistry {
 	}
 	
 	private static String getDefaultName(int aL, int bL, int cL) {
-		return getKey(aL,bL,cL);
+		StringBuilder builder = new StringBuilder();
+		
+		boolean first = true;
+		if ( aL > 0 ) {
+			builder.append(getALevel(aL,first));
+			first = false;
+		}
+		if ( bL > 0 ) {
+			builder.append(getBLevel(bL,first));
+			first = false;
+		}
+		if ( cL > 0 ) {
+			builder.append(getCLevel(cL,first));
+		}
+		builder.append(" (");
+		if ( aL > 0 ) {
+			builder.append(aL+"A");
+		}
+		if ( bL > 0 ) {
+			builder.append(bL+"B");
+		}
+		if ( cL > 0 ) {
+			builder.append(cL+"C");
+		}
+		builder.append(")");
+		
+		return Character.toUpperCase(builder.toString().charAt(0)) + builder.toString().substring(1);
+	}
+	
+	private static String getALevel(int aL, boolean first) {
+		String num = "";
+		if ( first || aL > 1 ) {
+			num = getNumeric(aL);
+		}
+		String name = "axi";
+		if ( num.length() > 0 && num.substring(num.length()-1, num.length()).equals("a") )
+			name = "xi";
+		
+		return num+name;
+	}
+	private static String getBLevel(int bL, boolean first) {
+		String num = "";
+		if ( first || bL > 1 ) {
+			num = getNumeric(bL);
+		}
+		String name = "ogen";
+		if ( num.length() > 0 && num.substring(num.length()-1, num.length()).equals("o") )
+			name = "gen";
+		
+		return num+name;
+	}
+	private static String getCLevel(int cL, boolean first) {
+		String num = "";
+		if ( first || cL > 1 ) {
+			num = getNumeric(cL);
+		}
+		String name = "cyde";
+		
+		return num+name;
+	}
+	
+	private static String getNumeric(int i) {
+		switch ( i ) {
+			case 1:
+				return "mono";
+			case 2:
+				return "di";
+			case 3:
+				return "tri";
+			case 4:
+				return "tetra";
+			case 5:
+				return "penta";
+			case 6:
+				return "hexa";
+			case 7:
+				return "hepta";
+			case 8:
+				return "octa";
+			case 9:
+				return "nona";
+			default:
+				return "";
+		}
 	}
 }
