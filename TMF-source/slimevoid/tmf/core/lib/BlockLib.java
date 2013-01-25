a988ef045ed625528349937292ef2e0a563da7d3
Eurymachus <reflex_ion@hotmail.com>
2013-01-25 22:10:54 +0000

    Tidy up BlockLib

--- TMF-source/slimevoid/tmf/core/lib/BlockLib.java
+++ TMF-source/slimevoid/tmf/core/lib/BlockLib.java
@@ -7,38 +7,39 @@ import slimevoid.tmf.blocks.ores.BlockTMFOre;
 
 public class BlockLib {
 
-	private static String BLOCK_PREFIX = 									"tmf.";
+	private static String BLOCK_PREFIX = 										"tmf.";
 
-	public static String BLOCK_REFINERY = 			BLOCK_PREFIX + 			"refinery";
-	public static String BLOCK_GRINDER = 			BLOCK_PREFIX + 			"grinder";
-	public static String BLOCK_GEOEQUIPMENT = 		BLOCK_PREFIX + 			"geoEquip";
+	public static final String BLOCK_REFINERY = 		BLOCK_PREFIX + 			"refinery";
+	public static final String BLOCK_GRINDER = 			BLOCK_PREFIX + 			"grinder";
+	public static final String BLOCK_GEOEQUIPMENT = 	BLOCK_PREFIX + 			"geoEquip";
+	public static final String BLOCK_AUTOMIXTABLE = 	BLOCK_PREFIX + 			"autoMixTable";
 	
-	private static String IDLE = 					".idle";
+	private static final String IDLE = 											".idle";
 
-	public static String REFINERY_IDLE = 			BLOCK_REFINERY + 		IDLE;
-	public static String GRINDER_IDLE = 			BLOCK_GRINDER + 		IDLE;
-	public static String GEOEQUIP_IDLE = 			BLOCK_GEOEQUIPMENT + 	IDLE;
+	public static final String REFINERY_IDLE = 			BLOCK_REFINERY + 		IDLE;
+	public static final String GRINDER_IDLE = 			BLOCK_GRINDER + 		IDLE;
+	public static final String GEOEQUIP_IDLE = 			BLOCK_GEOEQUIPMENT + 	IDLE;
 	
-	private static String ACTIVE = 					".active";
+	private static final String ACTIVE = 										".active";
 
-	public static String REFINERY_ACTIVE = 			BLOCK_REFINERY + 		ACTIVE;
-	public static String GRINDER_ACTIVE = 			BLOCK_GRINDER + 		ACTIVE;
-	public static String GEOEQUIP_ACTIVE = 			BLOCK_GEOEQUIPMENT + 	ACTIVE;
+	public static final String REFINERY_ACTIVE = 		BLOCK_REFINERY + 		ACTIVE;
+	public static final String GRINDER_ACTIVE = 		BLOCK_GRINDER + 		ACTIVE;
+	public static final String GEOEQUIP_ACTIVE = 		BLOCK_GEOEQUIPMENT + 	ACTIVE;
 	
-	private static final String ORE =				BLOCK_PREFIX + 			"ore.";
+	private static final String ORE =					BLOCK_PREFIX + 			"ore.";
 	
-	public static final String ORE_ARKITE = 		ORE + 					"arkite";
-	public static final String ORE_BISTITE = 		ORE + 					"bistite";
-	public static final String ORE_CROKERE = 		ORE + 					"crokere";
-	public static final String ORE_DERNITE = 		ORE + 					"dernite";
-	public static final String ORE_EGIOCLASE = 		ORE + 					"egioclase";
+	public static final String ORE_ARKITE = 			ORE + 					"arkite";
+	public static final String ORE_BISTITE = 			ORE + 					"bistite";
+	public static final String ORE_CROKERE = 			ORE + 					"crokere";
+	public static final String ORE_DERNITE = 			ORE + 					"dernite";
+	public static final String ORE_EGIOCLASE = 			ORE + 					"egioclase";
 
-	public static final String BLOCK_AUTOMIXTABLE = BLOCK_PREFIX + 			"autoMixTable";
+	private static final String TILE = 											"tile.";
 
-	public static final String TILE_REFINERY = 		"tile." + 				BLOCK_REFINERY;
-	public static final String TILE_GRINDER = 		"tile." + 				BLOCK_GRINDER;
-	public static final String TILE_GEOEQUIPMENT = 	"tile." + 				BLOCK_GEOEQUIPMENT;
-	public static final String TILE_AUTOMIXTABLE = 	"tile." + 				BLOCK_AUTOMIXTABLE;
+	public static final String TILE_REFINERY = 			TILE +	 				BLOCK_REFINERY;
+	public static final String TILE_GRINDER = 			TILE + 					BLOCK_GRINDER;
+	public static final String TILE_GEOEQUIPMENT = 		TILE + 					BLOCK_GEOEQUIPMENT;
+	public static final String TILE_AUTOMIXTABLE = 		TILE + 					BLOCK_AUTOMIXTABLE;
 	
 	private static List<BlockTMFOre> ores;
 	
