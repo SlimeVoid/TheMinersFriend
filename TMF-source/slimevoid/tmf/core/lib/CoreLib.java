fa367da0edb3f1a397f014f6eeea67eeb4601075
Eurymachus <reflex_ion@hotmail.com>
2014-02-16 13:03:52 +0000

    v0.0.1.9

--- TMF-source/slimevoid/tmf/core/lib/CoreLib.java
+++ TMF-source/slimevoid/tmf/core/lib/CoreLib.java
@@ -16,7 +16,7 @@ public class CoreLib {
     public static final String MOD_ID           = "TheMinersFriend";
     public static final String MOD_RESOURCES    = "tmf";
     public static final String MOD_NAME         = "The Miner's Friend";
-    public static final String MOD_VERSION      = "0.0.1.8";
+    public static final String MOD_VERSION      = "0.0.1.9";
     public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
     public static final String MOD_CHANNEL      = "TMF";
     public static final String PROXY_CLIENT     = "slimevoid.tmf.client.proxy.ClientProxy";
