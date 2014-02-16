a59e2d1b8f15aca03a2c749d00e0a05aa11fa21a
Eurymachus <reflex_ion@hotmail.com>
2014-02-16 04:21:05 +0000

    Updated toolbelt handler with exhaustion for mining mode

--- TMF-source/slimevoid/tmf/client/tickhandlers/PlayerToolBeltTickHandler.java
+++ TMF-source/slimevoid/tmf/client/tickhandlers/PlayerToolBeltTickHandler.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import slimevoid.tmf.core.helpers.ItemHelper;
+import slimevoid.tmf.core.lib.ConfigurationLib;
+import slimevoid.tmf.items.tools.ItemMiningToolBelt;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
 import cpw.mods.fml.relauncher.ReflectionHelper;
@@ -17,26 +19,54 @@ public class PlayerToolBeltTickHandler implements ITickHandler {
         if (type.equals(EnumSet.of(TickType.PLAYER))) {
             EntityPlayer entityplayer = (EntityPlayer) tickData[0];
             World world = entityplayer.worldObj;
-            if (entityplayer.isUsingItem()) {
-                ItemStack heldItem = entityplayer.getHeldItem();
-                ItemStack itemInUse = (ItemStack) ReflectionHelper.getPrivateValue(EntityPlayer.class,
-                                                                                   entityplayer,
-                                                                                   ItemHelper.getItemInUseFieldId(world,
-                                                                                                                  entityplayer));
-                if (!(heldItem == itemInUse)) {
-                    if (isHoldingAndUsingToolBelt(heldItem,
-                                                  itemInUse)
-                        && isUsingSelectedTool(heldItem,
-                                               itemInUse)) {
-                        ReflectionHelper.setPrivateValue(EntityPlayer.class,
-                                                         entityplayer,
-                                                         heldItem,
-                                                         ItemHelper.getItemInUseFieldId(world,
-                                                                                        entityplayer));
+            ItemStack heldItem = entityplayer.getHeldItem();
+            if (ItemHelper.isToolBelt(heldItem)) {
+                /**
+                 * Here we check if the tool belt is set as the item in use We
+                 * need to do this in order to satisfy the vanilla 'default'
+                 */
+                if (entityplayer.isUsingItem()) {
+                    ItemStack itemInUse = (ItemStack) ReflectionHelper.getPrivateValue(EntityPlayer.class,
+                                                                                       entityplayer,
+                                                                                       ItemHelper.getItemInUseFieldId(world,
+                                                                                                                      entityplayer));
+                    if (!(heldItem == itemInUse)) {
+                        if (isHoldingAndUsingToolBelt(heldItem,
+                                                      itemInUse)
+                            && isUsingSelectedTool(heldItem,
+                                                   itemInUse)) {
+                            ReflectionHelper.setPrivateValue(EntityPlayer.class,
+                                                             entityplayer,
+                                                             heldItem,
+                                                             ItemHelper.getItemInUseFieldId(world,
+                                                                                            entityplayer));
+                        }
+                    }
+                }
+                /**
+                 * Here we check if the player is swinging, if they are see if
+                 * they are holding a Tool Belt Once those checks are satisfied,
+                 * check if Mining mode is enabled if it is, start a counter
+                 * When the counter hits a critical value remove some saturation
+                 */
+                if (entityplayer.isSwingInProgress) {
+                    if (((ItemMiningToolBelt) heldItem.getItem()).isMiningModeEnabled(heldItem)) {
+                        entityplayer.addExhaustion(this.calculateAbsorption(world,
+                                                                            entityplayer,
+                                                                            heldItem));
                     }
                 }
             }
         }
+    }
+
+    private float calculateAbsorption(World world, EntityPlayer entityplayer, ItemStack heldItem) {
+        float progress = entityplayer.swingProgress;
+        // System.out.println("Swing: " + progress);
+        if (progress >= 0.5F) {
+            return ConfigurationLib.miningModeExhaustion;
+        }
+        return 0;
     }
 
     private boolean isUsingSelectedTool(ItemStack heldItem, ItemStack itemInUse) {
